113371f37b3b4e18773e8656726a57e45d453e1a
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-10-01 13:22:05 -0700

    algorithm-practice is updated

---
+++ algorithm-practice.js
@@ -0,0 +1,13 @@
+/* In mathematics, the factorial of a non-negative integer n, denoted by n!, 
+is the product of all positive integers less than or equal to n. 
+For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+Write a function to calculate factorial for a given input. 
+If input is below 0 or above 12 throw an exception of type a RangeError (JavaScript).
+*/
+
+const factorialNumber = function (num) {
+
+}
+
+// num > 0, recursion, reduce
